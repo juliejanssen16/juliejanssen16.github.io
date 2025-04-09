585d1fd93d38939b9e7ddc22dcd163f2e2d88938
Julie Janssen <julie.janssen@sintlucasedu.nl>
2025-04-09 10:33:29 +0200

    CookieBanner

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+let popUp = document.getElementById("cookiePopup");
+//When user clicks the accept button
+document.getElementById("acceptCookie").addEventListener("click", () => {
+    //Create date object
+    let d = new Date();
+    //Increment the current time by 1 minute (cookie will expire after 1 minute)
+    d.setMinutes(2 + d.getMinutes());
+    //Create Cookie withname = myCookieName, value = thisIsMyCookie and expiry time=1 minute
+    document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
+    //Hide the popup
+    popUp.classList.add("hide");
+    popUp.classList.remove("show");
+});
+//Check if cookie is already present
+const checkCookie = () => {
+    //Read the cookie and split on "="
+    let input = document.cookie.split("=");
+    //Check for our cookie
+    if (input[0] == "myCookieName") {
+        //Hide the popup
+        popUp.classList.add("hide");
+        popUp.classList.remove("show");
+    } else {
+        //Show the popup
+        popUp.classList.add("show");
+        popUp.classList.remove("hide");
+        console.log("showing cookey")
+    }
+};
+//Check if cookie exists when page loads
+// window.onload = () => {
+//     console.log("page loaded")
+//
+// };
+
 function openTab(evt, tabName) {
     // Declare all variables
     var i, tabContent, tablinks;
@@ -21,7 +56,12 @@ function openTab(evt, tabName) {
 
 // Add this code inside your script.js file
 window.onload = function () {
+    console.log("page loaded Two")
     openTab(event, 'HomePage'); // Call the openTab function with 'HomePage' as the initial tab
+    setTimeout(() => {
+        checkCookie();
+        console.log("checking cookey")
+    }, 1000);
 };
 var animateButton = function (e) {
     e.preventDefault(); // Invoke the preventDefault method
